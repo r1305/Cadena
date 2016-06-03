56cf720941c5e85bc55ed9b4eeba1e355df49d29
Julian <20101296@aloe.ulima.edu.pe>
2016-06-02 23:47:07 -0500

    apk final

--- app/src/main/java/com/example/julian/cadena/MainActivity.java
+++ app/src/main/java/com/example/julian/cadena/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.julian.cadena;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.ksoap2.SoapEnvelope;
@@ -16,13 +20,31 @@ public class MainActivity extends AppCompatActivity {
     private String URL="http://192.168.1.33:8080/WebService/Compara?WSDL";
     private final String SOAP_ACTION = "192.168.1.33:8080/WebService/";
     String METHOD_NAME="comparar";
+    EditText texto1;
+    EditText texto2;
+    TextView rpta;
+    Button calcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        comparar("hola","hola2");
+        texto1=(EditText)findViewById(R.id.txt1);
+        texto2=(EditText)findViewById(R.id.txt2);
+        rpta=(TextView)findViewById(R.id.rpta);
+        calcular=(Button)findViewById(R.id.calcular);
+
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comparar(texto1.getText().toString(),texto2.getText().toString());
+            }
+        });
+
+
+
+
     }
 
     public void comparar(final String texto1,final String texto2){
@@ -47,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
                     runOnUiThread (new Runnable(){ public void run() {
 
                         Toast.makeText(MainActivity.this,str,Toast.LENGTH_LONG).show();
+                        rpta.setText(str.toString());
 
                     }});
                 }catch (Exception e) {e.printStackTrace();}
